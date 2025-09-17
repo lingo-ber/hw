604ad4a67f837ba2dfb07bebacd92e49b560afb7
lingo-ber <lingo.ber@mail.ru>
2025-09-17 11:05:43 +0500

    Update database.py

--- homework/database.py
+++ homework/database.py
@@ -1,8 +1,8 @@
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
 DATABASE_URL = "sqlite+aiosqlite:///./recipe_book.db"
 engine = create_async_engine(DATABASE_URL, echo=True)
-async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+async_session = async_sessionmaker(engine, expire_on_commit=False)
 session = async_session()
 Base = declarative_base()
