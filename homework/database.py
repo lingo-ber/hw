8d0fca0d6b2b1ee8e546985746d0b6b98970c7fd
lingo-ber <lingo.ber@mail.ru>
2025-09-17 23:51:23 +0500

    Update database.py

--- homework/database.py
+++ homework/database.py
@@ -3,6 +3,6 @@ from sqlalchemy.orm import declarative_base
 
 DATABASE_URL = "sqlite+aiosqlite:///./recipe_book.db"
 engine = create_async_engine(DATABASE_URL, echo=True)
-async_session = async_sessionmaker(engine, expire_on_commit=False) # noga E501
+async_session = async_sessionmaker(engine, expire_on_commit=False)  # noga E501
 session = async_session()
 Base = declarative_base()
