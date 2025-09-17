7fc41ce3877f59579ebbeeb8df160c4074965b42
lingo-ber <lingo.ber@mail.ru>
2025-09-17 23:25:49 +0500

    Update general_test.py

--- tests/general_test.py
+++ tests/general_test.py
@@ -1,11 +1,24 @@
+import pytest
+
 from fastapi.testclient import TestClient
+from homework.database import Base, engine
 from homework.main import app
 
 
-client = TestClient(app)
+@pytest.fixture(scope="module", autouse=True)
+async def prepare_database():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
 
+@pytest.fixture(scope="module")
+def client():
+    with TestClient(app) as c:
+        yield c
 
-def test_requests():
+def test_requests(client):
     recipe_data = {
         "dish_name": "Пельмени",
         "cooking_time": 20,
